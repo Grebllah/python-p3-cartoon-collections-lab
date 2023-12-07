6efd023637ad256338522ffd31c15eeea4480873
James Hallberg <Grebllah81788@gmail.com>
2023-12-07 17:47:35 -0500

    python cartoon collections lab complete

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,25 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(names):
+    index = 0
+    for name in names:
+        order_number = 1 + index
+        print(f"{order_number}. {name}")
+        index = order_number
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(calls):
+    captain_planet = [f"{call.title()}" + "!" for call in calls]
+    return captain_planet
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) < 5:
+            pass
+        elif len(call) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(snacks):
+    ingredients = ["cheddar", "gouda", "camembert"]
+    for ingredient in snacks:
+        if ingredient in ingredients:
+            return ingredient
+    return None
